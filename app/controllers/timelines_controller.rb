bed3c587d31f75ab7b776a3a0b60a8aac2852155
clarkj99 <clark@stontech.net>
2020-04-19 01:55:08 -0400

    added breakthrough rating to timeline

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,38 +1,72 @@
 class TimelinesController < ApplicationController
+  before_action :set_user, only: [:show]
+
   def show
-    user = User.find_by(id: params[:user_id])
-    evaluations = user.evaluations
-    posts = user.posts
+    evaluations = @user.evaluations.order(rated_at: :desc).map do |evaluation|
+      { type: "evaluation",
+        data: evaluation }
+    end
+
+    posts = @user.posts.order(posted_at: :desc).map do |post|
+      { type: "post",
+        data: post }
+    end
 
     events = []
-    if user.github_username
-      events = github_events(user.github_username)
+
+    if @user.github_username
+      events = get_github_events.map do |event|
+        { type: "github",
+          data: event }
+      end
+    end
+
+    ratings = get_breakthrough_ratings.map do |rating|
+      { type: "rating_breakthrough",
+        data: rating }
     end
 
     # Combine arrays and sort by date descending
-    timeline = (evaluations + posts + events).sort do |a, b|
+    timeline = (evaluations + posts + events + ratings).sort do |a, b|
       # github event created_at date is stored in hash,
       #  rating and post are stored in a method
-      if a.is_a? Hash
-        first_date = a["created_at"]
+      if a[:data].is_a? Hash
+        first_date = a[:data]["created_at"]
       else
-        first_date = a.normalized_date
+        first_date = a[:"data"].normalized_date
       end
-      if b.is_a? Hash
-        second_date = b["created_at"]
+      if b[:data].is_a? Hash
+        second_date = b[:data]["created_at"]
       else
-        second_date = b.normalized_date
+        second_date = b[:data].normalized_date
       end
       second_date <=> first_date
     end
-
-    render json: timeline
+    render json: timeline[0..31]
   end
 
   private
 
-  def github_events(username)
-    url = "https://api.github.com/users/#{username}/events"
+  def set_user
+    @user = User.find_by(id: params[:user_id])
+  end
+
+  def get_breakthrough_ratings
+    ratings = @user.ratings.order(rated_at: :asc)
+    current_total = 0
+    current_count = 0
+    current_average = 0
+    ratings.select do |item|
+      current_count += 1
+      old_average = current_average
+      current_total += item.rating
+      current_average = current_total / (current_count)
+      (old_average < 4) && (current_average >= 4)
+    end
+  end
+
+  def get_github_events
+    url = "https://api.github.com/users/#{@user.github_username}/events"
     begin
       response = RestClient.get(url)
     rescue RestClient::ExceptionWithResponse => err
